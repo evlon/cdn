273b69031c3fddce051fb46be67cd99bb9e9b80a
evlon <liamgnc@gmail.com>
2019-12-05 07:03:49 +0800

    load fix

--- js/exam-inithook-load.js
+++ js/exam-inithook-load.js
@@ -1,6 +1,6 @@
 (function(){
     var myScript= document.createElement("script");
     myScript.type = "text/javascript";
-    myScript.src="https://cdn.jsdelivr.net/gh/evlon/cdn/js/exam-inithookz.js";
+    myScript.src="https://cdn.jsdelivr.net/gh/evlon/cdn/js/exam-inithook-mini.js";
     document.body.appendChild(myScript);
     })();

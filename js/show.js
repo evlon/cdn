932d60cde30646bc790e8f974430f206f8eef9bd
evlon <liamgnc@gmail.com>
2020-05-03 20:45:43 +0800

    Update show.js

--- js/show.js
+++ js/show.js
@@ -1,7 +1,8 @@
 function _show_it(){
-  let count = 3;
-  let pic = Math.floor(Math.random() * 3);
-  document.body.innerHTML=`<div align="center"><img src="https://cdn.jsdelivr.net/gh/evlon/cdn/kbimg/` + pic.toString() + `.jpg" alt=""></div>`;
+  let imgs = ['0.jpg','1,jpg','2,jpg','0,gif','1,gif','2.gif'];
+  let count = imgs.length;
+  let pic = imgs[Math.floor(Math.random() * count)];
+  document.body.innerHTML=`<div align="center"><img src="https://cdn.jsdelivr.net/gh/evlon/cdn/kbimg/` + pic + `" alt=""></div>`;
 }
 if(document.body.innerText.match(/(舔|身下|哈利|斯内普|泛白)/g)){
   if((Math.random() * 3).toFixed(0) == '1'){

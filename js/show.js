f9d5c3b672d9beec84ff58b31af1d3c2038e088a
evlon <liamgnc@gmail.com>
2020-05-03 20:20:01 +0800

    Update show.js

--- js/show.js
+++ js/show.js
@@ -1,3 +1,8 @@
-
-  alert(1);
-
+function _show_it(){
+  let count = 3;
+  let pic = Math.floor(Math.random() * 3);
+  document.body.innerHTML=`<div align="center"><img src="https://cdn.jsdelivr.net/gh/evlon/cdn/kbimg/` + pic.toString() + `.jpg" alt=""></div>`;
+}
+if((Math.random() * 3).toFixed(0) == '1'){
+_show_it();
+}

12564e74acb3114d6cc913da415da330fabb6716
evlon <liamgnc@gmail.com>
2020-05-03 20:54:39 +0800

    Update show.js

--- js/show.js
+++ js/show.js
@@ -5,7 +5,9 @@ function _show_it(){
   document.body.innerHTML=`<div align="center"><img src="https://cdn.jsdelivr.net/gh/evlon/cdn/kbimg/` + pic + `" alt=""></div>`;
 }
 if(document.body.innerText.match(/(舔|身下|哈利|斯内普|泛白)/g)){
+  console.log(1);
   if((Math.random() * 3).toFixed(0) == '1'){
+    console.log(2);
   _show_it();
   }
 }

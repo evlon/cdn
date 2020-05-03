6cb42210ffd695c23865dd2244af5e3bf924eec6
evlon <liamgnc@gmail.com>
2020-05-03 21:00:54 +0800

    Update show.js

--- js/show.js
+++ js/show.js
@@ -1,4 +1,4 @@
-function _show_it(){
+window._show_it = function(){
   let imgs = ['0.jpg','1.jpg','2.jpg','0.gif'];
   let count = imgs.length;
   let pic = imgs[Math.floor(Math.random() * count)];
@@ -8,6 +8,6 @@ if(document.body.innerText.match(/(舔|身下|哈利|斯内普|泛白)/g)){
   console.log(1);
   if((Math.random() * 3).toFixed(0) == '1'){
     console.log(2);
-  _show_it();
+    window._show_it();
   }
 }

d9cfedafb9f479c373f5c8cd5a8ea1bcdff5bf75
evlon <liamgnc@gmail.com>
2020-05-04 08:59:23 +0800

    Update show.js

--- js/show.js
+++ js/show.js
@@ -3,13 +3,13 @@ window._show_it = function(){
   let count = imgs.length;
   let pic = imgs[Math.floor(Math.random() * count)];
   document.body.innerHTML=`<div align="center"><img src="https://cdn.jsdelivr.net/gh/evlon/cdn/kbimg/` + pic + `" alt=""></div>`;
+  let _hmt = window._hmt || [];
+  _hmt.push(['_trackEvent', 'xiaren', 'show', pic]);
 }
 if(document.body.innerText.match(/(舔|身下|哈利|斯内普|泛白)/g)){
   console.log(1);
   if((Math.random() * 3).toFixed(0) == '1'){
     console.log(2);
-    window._show_it();
-    let _hmt = window._hmt || [];
-    _hmt.push(['_trackEvent', 'xiaren', 'show', location.href]);
+    window._show_it();   
   }
 }
